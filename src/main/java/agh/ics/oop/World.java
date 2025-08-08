dba46788367e5411ca5a610009054110118cd739
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-08-08 14:33:00 +0200

    Adding project files

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class World {
+
+    public static void main(String[] args) {
+        System.out.println("system wystartowal");
+//        World w = new World();
+        OptionsParser parser = new OptionsParser(args);
+        MoveDirection[] directions = parser.parse();
+        run(directions);
+        System.out.println("system zakonczyl dzialanie");
+    }
+
+    public static void run(MoveDirection[] directions){
+//        System.out.println("zwierzak idzie do przodu");
+
+        for(int i = 0; i < directions.length; i++ ){
+            switch (directions[i]){
+                case FORWARD -> System.out.println("zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("zwierzak idzie do tylu");
+                case LEFT -> System.out.println("zwierzak skreca w lewo");
+                case RIGHT -> System.out.println("zwierzak skreca w prawo");
+            }
+        };
+
+    }
+}
