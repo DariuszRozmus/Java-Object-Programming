4fd36d1e08a6f2309301f7a98849f6a6744ecbd4
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-08-13 17:35:29 +0200

    Feat: Add comprehensive tests for Vector2d arithmetic operations

--- src/test/java/agh/ics/oop/model/Vector2dTest.java
+++ src/test/java/agh/ics/oop/model/Vector2dTest.java
@@ -163,4 +163,84 @@ public class Vector2dTest {
         assertEquals(v13,v11.lowerLeft(v12));
         assertEquals(v13,v12.lowerLeft(v11));
     }
+
+    @Test
+    void add(){
+
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(2,2);
+        Vector2d v3 = new Vector2d(3,4);
+
+        Vector2d v4 = new Vector2d(0,0);
+        Vector2d v5 = new Vector2d(2,2);
+        Vector2d v6 = new Vector2d(2,2);
+
+        Vector2d v7 = new Vector2d(-1,3);
+        Vector2d v8 = new Vector2d(-2,-1);
+        Vector2d v9 = new Vector2d(-3,2);
+
+        Vector2d v10 = new Vector2d(0,0);
+        Vector2d v11 = new Vector2d(0,0);
+        Vector2d v12 = new Vector2d(0,0);
+
+        //when then
+        assertEquals(v3,v1.add(v2));
+        assertEquals(v3,v2.add(v1));
+
+        assertEquals(v6,v4.add(v5));
+        assertEquals(v6,v5.add(v4));
+
+        assertEquals(v9,v7.add(v8));
+        assertEquals(v9,v8.add(v7));
+    }
+
+    @Test
+    void subtract(){
+
+        //given
+        Vector2d v1 = new Vector2d(2,3);
+        Vector2d v2 = new Vector2d(1,2);
+        Vector2d v3 = new Vector2d(1,1);
+
+        Vector2d v4 = new Vector2d(2,3);
+        Vector2d v5 = new Vector2d(0,0);
+        Vector2d v6 = new Vector2d(2,3);
+
+        Vector2d v7 = new Vector2d(-1,-3);
+        Vector2d v8 = new Vector2d(-2,-5);
+        Vector2d v9 = new Vector2d(1,2);
+
+        Vector2d v10 = new Vector2d(-2,-3);
+        Vector2d v11 = new Vector2d(-1,-2);
+        Vector2d v12 = new Vector2d(-1,-1);
+
+        Vector2d v13 = new Vector2d(0,0);
+        Vector2d v14 = new Vector2d(0,0);
+        Vector2d v15 = new Vector2d(0,0);
+
+        //when then
+        assertEquals(v3,v1.subtract(v2));
+        assertEquals(v6,v4.subtract(v5));
+        assertEquals(v9,v7.subtract(v8));
+        assertEquals(v12,v10.subtract(v11));
+        assertEquals(v15,v13.subtract(v14));
+
+    }
+
+    @Test
+    void opposite(){
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(-1,-2);
+
+        Vector2d v3 = new Vector2d(-2,-1);
+        Vector2d v4 = new Vector2d(2,1);
+
+        Vector2d v5 = new Vector2d(0,0);
+        Vector2d v6 = new Vector2d(0,0);
+
+        assertEquals(v2,v1.opposite());
+        assertEquals(v4,v3.opposite());
+        assertEquals(v6,v5.opposite());
+    }
 }
