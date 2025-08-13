de697625341f1d1fda30c7d8d0bc3004da3f06e2
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-08-13 13:33:01 +0200

    test: Add comprehensive unit tests for Vector2d

---
+++ src/test/java/agh/ics/oop/model/Vector2dTest.java
@@ -0,0 +1,166 @@
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Vector2dTest {
+
+    @Test
+    void equals(){
+
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(1,2);
+        Vector2d v3 = new Vector2d(-1,2);
+        Vector2d v4 = new Vector2d(1,-2);
+        Vector2d v5 = new Vector2d(-1,-2);
+        Vector2d v6 = new Vector2d(0,0);
+        Vector2d v7 = new Vector2d(2,1);
+
+        //when then
+        assertTrue(v1.equals(v2));
+        assertFalse(v1.equals(v3));
+        assertFalse(v1.equals(v4));
+        assertFalse(v1.equals(v5));
+        assertFalse(v1.equals(v6));
+        assertFalse(v1.equals(v7));
+    }
+
+    @Test
+    void testToString(){
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(-1,2);
+        Vector2d v3 = new Vector2d(1,-2);
+        Vector2d v4 = new Vector2d(-1,-2);
+        Vector2d v5 = new Vector2d(0,0);
+
+        //when then
+        assertEquals("(1,2)", v1.toString());
+        assertEquals("(-1,2)", v2.toString());
+        assertEquals("(1,-2)", v3.toString());
+        assertEquals("(-1,-2)", v4.toString());
+        assertEquals("(0,0)", v5.toString());
+    }
+
+    @Test
+    void testPrecedes(){
+
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(2,3);
+        Vector2d v3 = new Vector2d(1,3);
+        Vector2d v4 = new Vector2d(2,2);
+        Vector2d v5 = new Vector2d(0,0);
+
+        //when then
+        assertTrue(v1.precedes(v1));
+        assertTrue(v1.precedes(v2));
+        assertTrue(v1.precedes(v3));
+        assertTrue(v1.precedes(v4));
+        assertFalse(v1.precedes(v5));
+    }
+
+    @Test
+    void testFollows(){
+
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(1,1);
+        Vector2d v3 = new Vector2d(0,2);
+        Vector2d v4 = new Vector2d(0,0);
+
+        Vector2d v5 = new Vector2d(2,2);
+        Vector2d v6 = new Vector2d(1,3);
+        Vector2d v7 = new Vector2d(3,4);
+
+        //when then
+        assertTrue(v1.follows(v1));
+        assertTrue(v1.follows(v2));
+        assertTrue(v1.follows(v3));
+        assertTrue(v1.follows(v4));
+
+        assertFalse(v1.follows(v5));
+        assertFalse(v1.follows(v6));
+        assertFalse(v1.follows(v7));
+    }
+
+    @Test
+    void upperRight(){
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(2,2);
+        Vector2d v3 = new Vector2d(2,2);
+
+        Vector2d v4 = new Vector2d(1,1);
+        Vector2d v5 = new Vector2d(2,2);
+        Vector2d v6 = new Vector2d(2,2);
+
+        Vector2d v7 = new Vector2d(0,0);
+
+        Vector2d v8 = new Vector2d(-1,-2);
+        Vector2d v9 = new Vector2d(-2,-1);
+        Vector2d v10 = new Vector2d(-1,-1);
+
+        Vector2d v11 = new Vector2d(-1,-1);
+        Vector2d v12 = new Vector2d(-2,-2);
+        Vector2d v13 = new Vector2d(-1,-1);
+
+        //when then
+        assertEquals(v3,v2.upperRight(v2));
+
+        assertEquals(v3, v1.upperRight(v2));
+        assertEquals(v3, v2.upperRight(v1));
+
+        assertEquals(v6, v4.upperRight(v5));
+        assertEquals(v6, v5.upperRight(v4));
+
+        assertEquals(v7, v7.upperRight(v7));
+
+        assertEquals(v10, v8.upperRight(v9));
+        assertEquals(v10, v9.upperRight(v8));
+
+        assertEquals(v13, v11.upperRight(v12));
+        assertEquals(v13, v12.upperRight(v11));
+    }
+
+    @Test
+    void lowerLeft(){
+
+        //given
+        Vector2d v1 = new Vector2d(1,2);
+        Vector2d v2 = new Vector2d(2,2);
+        Vector2d v3 = new Vector2d(1,1);
+
+        Vector2d v4 = new Vector2d(1,1);
+        Vector2d v5 = new Vector2d(2,2);
+        Vector2d v6 = new Vector2d(1,1);
+
+        Vector2d v7 = new Vector2d(0,0);
+
+        Vector2d v8 = new Vector2d(-1,-2);
+        Vector2d v9 = new Vector2d(-2,-1);
+        Vector2d v10 = new Vector2d(-1,-1);
+
+        Vector2d v11 = new Vector2d(-1,-1);
+        Vector2d v12 = new Vector2d(-2,-2);
+        Vector2d v13 = new Vector2d(-1,-1);
+
+        //when then
+        assertEquals(v3,v1.lowerLeft(v1));
+
+        assertEquals(v3,v1.lowerLeft(v2));
+        assertEquals(v3,v2.lowerLeft(v1));
+
+        assertEquals(v6,v4.lowerLeft(v5));
+        assertEquals(v6,v5.lowerLeft(v4));
+
+        assertEquals(v7,v7.lowerLeft(v7));
+
+        assertEquals(v10,v8.lowerLeft(v9));
+        assertEquals(v10,v9.lowerLeft(v8));
+
+        assertEquals(v13,v11.lowerLeft(v12));
+        assertEquals(v13,v12.lowerLeft(v11));
+    }
+}
