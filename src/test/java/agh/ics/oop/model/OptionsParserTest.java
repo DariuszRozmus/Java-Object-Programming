298826d819cafc8cba5b48a44e43407ec9f6ef7c
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-09-19 14:02:58 +0200

    test: Add unit tests for OptionsParser

---
+++ src/test/java/agh/ics/oop/model/OptionsParserTest.java
@@ -0,0 +1,82 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.model.MoveDirection;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OptionsParserTest {
+
+    @Test
+    public void testParseOptions() {
+
+        //given
+        String s1 = new String("a");
+        String s2 = new String("b");
+        String s3 = new String("f");
+        String s4 = new String("r");
+        String s5 = new String("");
+        String s6 = new String(" ");
+        String s7 = new String("l");
+
+        String[] moves = new String[] {s1, s2, s3, s4, s5, s6, s7};
+
+        OptionsParser parser = new OptionsParser();
+
+        MoveDirection[] moveDirections = new MoveDirection[] {MoveDirection.BACKWARD, MoveDirection.FORWARD,
+                MoveDirection.RIGHT, MoveDirection.LEFT};
+
+        //when
+        MoveDirection[] result = parser.parse(moves);
+
+        //then
+        assertArrayEquals(moveDirections, result);
+    }
+
+    @Test
+    public void testParseCorrectDirections() {
+
+        //given
+        String s1 = new String("b");
+        String s2 = new String("l");
+        String s3 = new String("f");
+        String s4 = new String("r");
+
+
+        String[] moves = new String[] {s1, s2, s3, s4};
+
+        OptionsParser parser = new OptionsParser();
+
+        MoveDirection[] moveDirections = new MoveDirection[] {MoveDirection.BACKWARD, MoveDirection.LEFT,
+                MoveDirection.FORWARD, MoveDirection.RIGHT};
+
+        //when
+        MoveDirection[] result = parser.parse(moves);
+
+        //then
+        assertArrayEquals(moveDirections, result);
+    }
+
+    @Test
+    public void testParseIncorrectDirections() {
+
+        //given
+        String s1 = new String("");
+        String s2 = new String(" ");
+        String s3 = new String("'");
+        String s4 = new String(";");
+
+
+        String[] moves = new String[] {s1, s2, s3, s4};
+
+        OptionsParser parser = new OptionsParser();
+
+        MoveDirection[] moveDirections = new MoveDirection[] {};
+
+        //when
+        MoveDirection[] result = parser.parse(moves);
+
+        //then
+        assertArrayEquals(moveDirections, result);
+    }
+}
