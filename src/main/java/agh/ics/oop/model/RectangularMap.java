5dc9aa6521f3459b4b6dd18458dba4dbc7836a2d
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-09-27 21:14:06 +0200

    feat: Implement RectangularMap as WorldMap

--- src/main/java/agh/ics/oop/model/RectangularMap.java
+++ src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -1,4 +1,49 @@
 package agh.ics.oop.model;
 
-public class RectangularMap {
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap {
+
+    private HashMap<Vector2d, Animal> animals;
+    private Vector2d upcorner;
+
+    public RectangularMap(int width, int height) {
+        this.animals = new HashMap<>();
+        this.upcorner = new Vector2d(width, height);
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        if (!animals.containsKey(animal.getPosition())) {
+            animals.put(animal.getPosition(), animal);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void move(Animal animal, MoveDirection direction) {
+        if (animals.containsKey(animal.getPosition())) {
+            var oldPosition = animal.getPosition();
+            animal.move(direction);
+            animals.remove(oldPosition);
+            animals.put(animal.getPosition(), animal);
+        }
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        return false;
+    }
+
+    @Override
+    public Animal objectAt(Vector2d position) {
+        return null;
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return false;
+    }
 }
