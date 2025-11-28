13cdde20c8f11340da7b9e1be6ecd4657e93290d
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-11-28 14:16:06 +0100

    feat: Implement GrassField world map with random grass generation

--- src/main/java/agh/ics/oop/model/GrassField.java
+++ src/main/java/agh/ics/oop/model/GrassField.java
@@ -2,44 +2,40 @@ package agh.ics.oop.model;
 
 import agh.ics.oop.model.util.MapVisualizer;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class GrassField implements WorldMap{
+import static java.lang.Math.sqrt;
 
-    private int grassCount;
-    private final Map<Vector2d, Animal> animals;
-    private final Vector2d DOWNCORNER = new Vector2d(0, 0);
-    private final Vector2d UPCORNER = new Vector2d(Integer.MAX_VALUE, Integer.MAX_VALUE);
-    private final MapVisualizer mapVisualizer;
+//Tree set dobry pomysł, bo zachowuje kolejność, także można zawsze sprawdzić pierwszy i ostatni
+// Collection<WorldElement> zamiast Collection<? extends WorldElement> bo to drugie może się ewoluować do WorldElement, GrassElement, Animal
+public class GrassField extends AbstractWorldMap{
 
-    public GrassField(int grassCount, Map<Vector2d, Animal> animals, MapVisualizer mapVisualizer) {
-        this.grassCount = grassCount;
-        this.animals = animals;
-        this.mapVisualizer = mapVisualizer;
+    private final Map<Vector2d, Grass> grasses;
+
+    public GrassField(int grassCount) {
+        super(new HashMap<Vector2d, Animal>());
+        this.grasses = new HashMap<>();
+        this.UPCORNER = new Vector2d(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+        int maxWidth = (int) (sqrt(10 * grassCount));
+        int maxHeight = (int) (sqrt(10 * grassCount));
+        RandomPositionGenerator randomPositionGenerator = new RandomPositionGenerator(maxWidth, maxHeight, grassCount);
+        for (Vector2d position : randomPositionGenerator) {
+            grasses.put(position, new Grass(position));
+        }
     }
-
-    @Override
-    public boolean place(Animal animal) {
-        return false;
-    }
-
-    @Override
-    public void move(Animal animal, MoveDirection direction) {
-
-    }
-
-    @Override
-    public boolean isOccupied(Vector2d position) {
-        return false;
-    }
-
+// Jeśli nadpisujemy metodę, to dobrze użyć super.metodaAbstractClassy
     @Override
     public WorldElement objectAt(Vector2d position) {
-        return null;
+        WorldElement animal = super.objectAt(position);
+        return animal == null ? grasses.get(position) : null;
     }
+    // TODO Nadpisać getElements (stworzyć XD)
 
-    @Override
-    public boolean canMoveTo(Vector2d position) {
-        return false;
-    }
+//    @Override
+//    public List<WorldElement> getElements() {
+//
+//    }
 }
