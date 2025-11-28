5d48da50dd293e39633ed3019613df619c8c4258
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-11-28 14:16:07 +0100

    test: Add GrassField tests and update AnimalTest

---
+++ src/test/java/agh/ics/oop/model/GrassFieldTest.java
@@ -0,0 +1,14 @@
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+
+public class GrassFieldTest {
+
+    @Test
+    public void CounterGrassOnMap() {
+        //given
+        GrassField grassField = new GrassField(10);
+
+
+    }
+}
