37d2f0f7df2e1106e78f1b34496709e3cf454422
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-11-28 14:16:05 +0100

    refactor: Make RectangularMap extend AbstractWorldMap

--- src/main/java/agh/ics/oop/model/RectangularMap.java
+++ src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -6,41 +6,8 @@ import java.util.*;
 
 public class RectangularMap extends AbstractWorldMap {
 
-    private final Map<Vector2d, Animal> animals;
-    private final Vector2d DOWNCORNER = new Vector2d(0, 0);
-    private final Vector2d UPCORNER;
-    private final MapVisualizer mapVisualizer;
-
-
     public RectangularMap(Vector2d upcorner) {
-        super(new HashMap<>(), u, Vector2d upcorner, MapVisualizer mapVisualizer);
-
-
+        super(new HashMap<Vector2d, Animal>());
+        this.UPCORNER = upcorner;
     }
-
-    @Override
-    public boolean canMoveTo(Vector2d position) {
-        return UPCORNER.follows(position) && DOWNCORNER.precedes(position);
-    }
-
-
-
-//    private void removeAnimal(Animal animal, Vector2d position) {
-//        List<Animal> animalList = animals.get(position);
-//        if(animalList != null) {
-//            animalList.remove(animal);
-//            if(animalList.isEmpty()) {
-//                animals.remove(animal.getPosition());
-//            }
-//        }
-//    }
-//
-//    private void addAnimal(Animal animal) {
-//        animals.computeIfAbsent(animal.getPosition(), x -> new ArrayList<>()).add(animal);
-//    }
-
-
-
-
-
 }
