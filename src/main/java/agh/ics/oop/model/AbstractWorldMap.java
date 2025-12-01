11294c8d140619eee8218ecb6f29f7e92fde1b56
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-12-01 13:18:45 +0100

    feat: Generalize AbstractWorldMap to store WorldElements

--- src/main/java/agh/ics/oop/model/AbstractWorldMap.java
+++ src/main/java/agh/ics/oop/model/AbstractWorldMap.java
@@ -6,41 +6,41 @@ import java.util.Map;
 
 abstract class AbstractWorldMap implements WorldMap {
 
-    protected final Map<Vector2d, Animal> animals;
+    protected final Map<Vector2d, WorldElement> worldElements;
     protected final Vector2d DOWNCORNER = new Vector2d(0, 0);
     protected Vector2d UPCORNER;
     protected final MapVisualizer mapVisualizer;
 
-    protected AbstractWorldMap(Map<Vector2d, Animal> animals) {
-        this.animals = animals;
+    protected AbstractWorldMap(Map<Vector2d, WorldElement> worldElements) {
+        this.worldElements = worldElements;
         this.mapVisualizer = new MapVisualizer(this);
     }
 
     public boolean place(Animal animal) {
         if(UPCORNER.follows(animal.getPosition()) && DOWNCORNER.precedes(animal.getPosition())) {
-            animals.put(animal.getPosition(), animal);
+            worldElements.put(animal.getPosition(), animal);
             return true;
         }
         return false;
     }
 
     public void move(Animal animal, MoveDirection direction) {
-        if (animals.containsKey(animal.getPosition())) {
+        if (worldElements.containsKey(animal.getPosition())) {
             var oldPosition = animal.getPosition();
             animal.move(direction,this);
             if (!oldPosition.equals(animal.getPosition())) {
-                animals.remove(oldPosition);
-                animals.put(animal.getPosition(), animal);
+                worldElements.remove(oldPosition);
+                worldElements.put(animal.getPosition(), animal);
             }
         }
     }
 
     public boolean isOccupied(Vector2d position) {
-        return animals.containsKey(position);
+        return worldElements.containsKey(position);
     }
 
     public WorldElement objectAt(Vector2d position) {
-        return animals.getOrDefault(position, null);
+        return worldElements.getOrDefault(position, null);
     }
 
     public boolean canMoveTo(Vector2d position) {
