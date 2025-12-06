be01c2c4766ee1de412d0348815bc1cdb5601e65
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-12-06 08:55:59 +0100

    Feat: Add unit tests for GrassField

---
+++ src/test/java/agh/ics/oop/model/GrassFieldTest.java
@@ -0,0 +1,28 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.Simulation;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GrassFieldTest {
+
+    @Test
+    public void getElements() {
+        var grassField = new GrassField(10);
+        var positions = List.of(new Vector2d(0, 0), new Vector2d(3, 3), new Vector2d(4, 4));
+        var parser = new OptionsParser();
+        String[] input = {"f", "b", "f", "b"};
+
+        var moveDirections = parser.parse(input);
+        var simulation = new Simulation(positions, moveDirections, grassField);
+        simulation.run(grassField);
+
+        var elements = grassField.getElements();
+
+        assertEquals(13, elements.size());
+    }
+}
