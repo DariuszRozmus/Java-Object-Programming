f36841488b0dd4a8db140e69fb6d8e0d9282b34e
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-12-06 08:55:58 +0100

    Refactor: Improve RandomPositionGenerator iteration state management

--- src/main/java/agh/ics/oop/model/RandomPositionGenerator.java
+++ src/main/java/agh/ics/oop/model/RandomPositionGenerator.java
@@ -4,17 +4,19 @@ import java.util.*;
 
 public class RandomPositionGenerator implements Iterable<Vector2d> {
     private final List<Vector2d> positions;
-    private int count;
+    private int amount;
     private final Random random = new Random();
+    private int counter = 0;
 
-    public RandomPositionGenerator(int width, int height, int count) {
+    public RandomPositionGenerator(int width, int height, int amount) {
         positions = new ArrayList<>();
         for (int x = 0; x <= width; x++) {
             for (int y = 0; y <= height; y++) {
-                positions.add(new Vector2d(x, y));
+                Vector2d position = new Vector2d(x, y);
+                positions.add(position);
             }
         }
-        this.count = count;
+        this.amount = amount;
     }
 
     @Override
@@ -22,15 +24,15 @@ public class RandomPositionGenerator implements Iterable<Vector2d> {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                return count > 0;
+                return amount > counter ;
             }
 
             @Override
             public Vector2d next() {
-                int i = random.nextInt(count);
+                int i = random.nextInt(positions.size() - counter);
                 Vector2d pos = positions.get(i);
-                Collections.swap(positions, i, count - 1);
-                count--;
+                Collections.swap(positions, i, positions.size() - counter - 1);
+                counter++;
                 return pos;
             }
         };
