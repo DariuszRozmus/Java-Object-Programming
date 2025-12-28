2b37a1d7123dab1b82cbb4c9102d2a32c78afe19
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-12-28 22:00:13 +0100

    refactor(World): Configure and scale simulation setup for concurrent execution

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,6 +2,9 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.*;
 import agh.ics.oop.Simulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static agh.ics.oop.model.MoveDirection.*;
@@ -11,9 +14,15 @@ public class World {
     public static void main(String[] args) {
         System.out.println("system wystartowal");
         OptionsParser parser = new OptionsParser();
-        String[] arguments = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f",
-                "f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f",
-                "f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f"};
+
+        int repeatCount = 400000;
+        String[] baseArguments = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f"};
+        String[] arguments = new String[baseArguments.length * repeatCount];
+
+        for (int i = 0; i < repeatCount; i++) {
+            System.arraycopy(baseArguments, 0, arguments, i * baseArguments.length, baseArguments.length);
+        }
+
         List<MoveDirection> directions;
         try {
             directions = parser.parse(arguments);
@@ -24,12 +33,18 @@ public class World {
         }
         List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,3), new Vector2d(4,4));
         ConsoleMapDisplay consoleMapDisplay = new ConsoleMapDisplay();
-        Simulation simulationGrassField = new Simulation(positions, directions, new GrassField(10), consoleMapDisplay);
-        Simulation simulationRectangularMap = new Simulation(positions, directions, new RectangularMap(new Vector2d(10,10)), consoleMapDisplay);
-        var mapList = List.of(simulationGrassField, simulationRectangularMap);
+//        Simulation simulationGrassField = new Simulation(positions, directions, new GrassField(10), consoleMapDisplay);
+//        Simulation simulationRectangularMap = new Simulation(positions, directions, new RectangularMap(new Vector2d(10,10)), consoleMapDisplay);
+        List<Simulation> mapList = new ArrayList<>();
+        for (int i = 0; i<6; i++){
+            Simulation simulation1 = new Simulation(positions, directions, new GrassField(10), consoleMapDisplay);
+            Simulation simulation2 = new Simulation(positions, directions, new RectangularMap(new Vector2d(10,10)), consoleMapDisplay);
+            mapList.add(simulation1);
+            mapList.add(simulation2);
+        }
         SimulationEngine engine = new SimulationEngine(mapList);
-        engine.runAsync();
         try {
+            engine.runAsyncThreadPool();
             engine.awaitSimulationsEnd();
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
