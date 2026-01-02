1bcc2c005747dad255bbec53b9e053b27cb3dec2
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2026-01-02 19:56:20 +0100

    feat: Add UI elements for simulation control and feedback

--- src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,7 +1,9 @@
 package agh.ics.oop.presenter;
 
+import agh.ics.oop.Simulation;
 import agh.ics.oop.model.WorldMap;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class SimulationPresenter {
@@ -10,12 +12,34 @@ public class SimulationPresenter {
     @FXML
     private Label infoLabel;
 
-    public SimulationPresenter(){}
+    @FXML
+    private Label moveInfoLabel;
 
+    @FXML
+    private Button startButton;
+
+    private Simulation simulation;
+
+    public SimulationPresenter(){
+    }
+
+    @FXML
+    void initialize(){
+        startButton.setOnAction(actionEvent -> {
+            new Thread(simulation).start();
+            startButton.setDisable(true);
+            });
+    }
     public void drawMap(){
         infoLabel.setText(worldMap.toString());
     }
+    public void showMoves(String text){
+        moveInfoLabel.setText(text);
+    }
     public void setWorldMap(WorldMap worldMap) {
         this.worldMap = worldMap;
+    }
+    public void setSimulation(Simulation simulation){
+        this.simulation = simulation;
     }
 }
